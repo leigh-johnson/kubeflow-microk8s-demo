d674199ec3de84fee812bc14a7f07c2f6bfcf016
Leigh Johnson <hi@leighjohnson.me>
2018-11-19 11:27:57 -0800

    fix go installation

--- install-go.sh
+++ install-go.sh
@@ -1,12 +1,14 @@
 echo "Downloading and installing go1.11.2.linux-amd64.tar.gz"
 wget https://dl.google.com/go/go1.11.2.linux-amd64.tar.gz /home/multipass/go1.11.2.linux-amd64.tar.gz
 tar -xvf go1.11.2.linux-amd64.tar.gz
-sudo chown -R root:root ./go
-sudo mv go /usr/local
 echo "Removing go1.11.2.linux-amd64.tar.gz"
 rm go1.11.2.linux-amd64.tar.gz
-# build ksonnet from source
+mkdir -p /home/multipass/work
+source /multipass/dev.env
+
+
 echo "Installing ksonnet/ksonnet:master"
 go get github.com/ksonnet/ksonnet
 cd $GOPATH/src/github.com/ksonnet/ksonnet
+sudo apt-get install make
 make install
